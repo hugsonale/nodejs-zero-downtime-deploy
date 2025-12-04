7cb302688245eb558fe0f863c0bb612e129fa18d
devops4sure <adeboyejesutofunmi001@gmail.com>
2025-12-04 12:49:21 +0100

    Finished commit

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const version = process.env.APP_VERSION || 'v1';
+const version = process.env.APP_VERSION || 'v2';
 
 app.get('/', (req, res) => {
   res.send(`Hello from Node.js app! Version: ${version}`);
